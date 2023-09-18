2576611dc2e0b57f55361890d9daa09c2ef2722c
AgriDulce7u7 <142194231+AgriDulce7u7@users.noreply.github.com>
2023-09-18 16:09:40 -0500

    Creación de atributos de clases

--- co.edu.uniquindio.tiendaRopa/src/main/java/org/co/edu/uniquindio/tiendaRopa/Model/Cliente.java
+++ co.edu.uniquindio.tiendaRopa/src/main/java/org/co/edu/uniquindio/tiendaRopa/Model/Cliente.java
@@ -1,4 +1,22 @@
 package org.co.edu.uniquindio.tiendaRopa.Model;
 
 public class Cliente {
+    private String nombreCompleto;
+    private long numeroIdentificacion;
+    private String sexo;
+    private int prendasCompradas;
+    private String fechaUltimaCompra;
+
+    /* Constructor Vacío */
+    public Cliente() {
+    }
+
+    /* Constructor */
+    public Cliente(String nombreCompleto, long numeroIdentificacion, String sexo, int prendasCompradas, String fechaUltimaCompra) {
+        this.nombreCompleto = nombreCompleto;
+        this.numeroIdentificacion = numeroIdentificacion;
+        this.sexo = sexo;
+        this.prendasCompradas = prendasCompradas;
+        this.fechaUltimaCompra = fechaUltimaCompra;
+    }
 }
--- co.edu.uniquindio.tiendaRopa/src/main/java/org/co/edu/uniquindio/tiendaRopa/Model/Prenda.java
+++ co.edu.uniquindio.tiendaRopa/src/main/java/org/co/edu/uniquindio/tiendaRopa/Model/Prenda.java
@@ -1,4 +1,27 @@
 package org.co.edu.uniquindio.tiendaRopa.Model;
 
 public class Prenda {
+    private String tipoPrenda;
+    private String referencia;
+    private String tipoCliente;
+    private String talla;
+    private String color;
+    private double precio;
+    private int cantidadDisponible;
+
+    /* Constructor Vacío */
+    public Prenda() {
+    }
+
+    /* Constructor */
+    public Prenda(String tipoPrenda, String referencia, String tipoCliente, String talla, String color, double precio, int cantidadDisponible) {
+        this.tipoPrenda = tipoPrenda;
+        this.referencia = referencia;
+        this.tipoCliente = tipoCliente;
+        this.talla = talla;
+        this.color = color;
+        this.precio = precio;
+        this.cantidadDisponible = cantidadDisponible;
+    }
+
 }
